80a53ece88c19dbe97d2951773f09e6902af4bb0
Jose Isaula <isaulajosue504@gmail.com>
2022-05-09 18:09:11 -0600

    Programa 1: Holamundo.cpp

---
+++ Unidad I/Holamundo.cpp
@@ -0,0 +1,21 @@
+/*
+    Sintaxis: Son las reglas que se utilizan para escribir un programa en c++.
+
+    Sintaxis en c++:
+    1. Incluir o importar librerias.
+    2. Declarar la funcion principal.
+    3. Declarar las variables.
+    4. Escribir el codigo del programa en la funcion principal.
+*/
+
+#include <iostream> //Importar libreria para escribir en pantalla.
+
+using namespace std; //Se utiliza para no escribir std::cout.
+
+int main() { //Funcion principal.
+    cout << "Hola mundo! \n"; //Se escribe en pantalla.
+    
+    
+    return 0; //Se retorna un 0 para indicar que el programa termino correctamente.
+}
+
