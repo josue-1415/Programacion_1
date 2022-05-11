25990fd3b456dfdbc7f306d3cec88782990deb19
Jose Isaula <isaulajosue504@gmail.com>
2022-05-11 10:47:29 -0600

    Modificacion Programa 2: 2.Entrada_de_Datos.cpp

--- Unidad I/2.Entrada_de_Datos.cpp
+++ Unidad I/2.Entrada_de_Datos.cpp
@@ -3,14 +3,33 @@
 using namespace std; //Se utiliza para no escribir std::cout.
 
 int main() {
-    string nombre; //Declarar una variable de tipo string (Cadena de texto).
-    int edad; //Declarar una variable de tipo int (Entero).
+
+    /*  Variables: Son entidades que nos permiten almacenar informacion y poder hacer
+        operaciones con ellas.
+
+        Sintaxis: TipoDeDato NombreDeLaVariable;
+
+        TipoDeDato: Es el tipo de dato que va a almacenar la variable.
+        NombreDeLaVariable: Es el nombre que le daremos a la variable.
+
+        ---------------------------------------------------------------------------------
+        ---------------------------------TIPOS DE DATOS----------------------------------
+        ---------------------------------------------------------------------------------
+
+        Las variables se declaran con la palabra reservada "int", "double", "char",
+        "bool", "float", "long", "short", "unsigned", "signed", "void", "const",
+        "volatile", "static", "auto", "register", "extern", "typedef", "union",
+        "struct", "enum".
+    */
+
+    string nombre; //Declarar una variable de tipo string (String : Almacena una Cadena de texto).
+    int edad; //Declarar una variable de tipo int (Almacena un valor Entero).
 
     cout<<"Bienvenido a mi segundoo programa en c++ \n";
     cout<<"Ingrese su nombre: "<<endl;
-    cin>>nombre;
+    cin>>nombre;    //cin>>nombre es una forma de leer una variable de tipo string. 
     cout<<"Ingrese su edad: "<<endl;
-    cin>>edad;
+    cin>>edad;    //cin>>edad es una forma de leer una variable de tipo int.
     cout<<"Hola "<<nombre<<"!! , su edad es de "<<edad<<" años. \n";
 
     return 0;
