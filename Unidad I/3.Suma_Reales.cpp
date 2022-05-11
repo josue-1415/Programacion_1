a85eb5abd4eb4afef8bd18bb458b65aa7e278ad4
Jose Isaula <isaulajosue504@gmail.com>
2022-05-11 10:42:42 -0600

    Programa 3: 3.Suma_Reales.cpp

---
+++ Unidad I/3.Suma_Reales.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float n1, n2, suma;   // float es un tipo de dato que almacena un numero real.
+
+    cout<<"Ingrese el primer numero  : ";
+    cin>>n1;
+    cout<<"Ingrese el segundo numero : ";
+    cin>>n2;
+    // suma = n1 + n2;
+    cout<<"La suma de los numeros es : "<<n1 + n2<<endl;
+
+    return 0;
+}
