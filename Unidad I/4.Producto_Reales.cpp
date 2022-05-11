a1f7ce7798cb75610b857496be18cbbab1964c60
Jose Isaula <isaulajosue504@gmail.com>
2022-05-11 10:43:47 -0600

    Programa 4: 4.Producto_Reales.cpp

---
+++ Unidad I/4.Producto_Reales.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float n1, n2, Producto;   // float es un tipo de dato que almacena un numero real.
+
+    cout<<"Ingrese el primer numero  : ";
+    cin>>n1;
+    cout<<"Ingrese el segundo numero : ";
+    cin>>n2;
+    Producto = n1 * n2;
+    cout<<"La suma de los numeros es : "<<Producto<<endl;
+
+    return 0;
+}
